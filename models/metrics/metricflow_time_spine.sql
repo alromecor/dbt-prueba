27a6b1d71afc4f8f72c415ae576d7450e3f43fb3
Álvaro Romero <alvaro.romero@bosonit.com>
2025-03-21 11:16:24 +0000

    a

--- models/metrics/metricflow_time_spine.sql
+++ models/metrics/metricflow_time_spine.sql
@@ -2,8 +2,8 @@
 --for BQ adapters use "DATE('01/01/2000','mm/dd/yyyy')"
 with days as (
     {{dbt_utils.date_spine('day'
-    , "parse_date('01/01/2000','mm/dd/yyyy')"
-    , "parse_date('01/01/2027','mm/dd/yyyy')"
+    , "parse_date('mm/dd/yyyy','01/01/2000')"
+    , "parse_date('mm/dd/yyyy','01/01/2027')"
     )
     }}
 ),
